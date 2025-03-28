b08be6d81ee7f165c43011dabafd53a3f5bddf7c
David Bondy <bondydaa@gmail.com>
2025-03-27 21:46:17 -0600

    remove old configs that i possibly dont need now

--- nvim/lua/config/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-local colorscheme = "PaperColor"
-local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-vim.o.background = "light" -- or "light" for light mode
-if not ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
